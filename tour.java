66c92a3c80a35c86991ba39cd23f877ff81f6d7b
VisionMass <DemomanKung2547@gmail.com>
2023-05-10 16:15:43 +0700

    add perm ma

--- tour.java
+++ tour.java
@@ -6,5 +6,14 @@ import javax.swing.border.SoftBevelBorder;
 
 public class tour extends JPanel {
 
+    void welcomePage(JFrame frame) {
+        setSize(1280, 720);
+        setLayout(null);
+        frame.setContentPane(this);
+        frame.setResizable(false); // ปรับขนาดหน้าต่างให้พอดีกับเนื้อที่ที่ได้กำหนดไว้
+        frame.setLocationRelativeTo(null); // กำหนดให้หน้าต่างอยู่ตรงกลางจอ
+        frame.setVisible(true); // แสดงหน้าต่าง
+
+    }
 
 }
